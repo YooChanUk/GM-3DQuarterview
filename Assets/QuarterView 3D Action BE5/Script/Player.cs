95d004126d1cd05779f17a48ecafe5012e1dfd3c
YooChanUk <psback101@gmail.com>
2021-12-13 19:48:04 +0900

    2

--- Assets/QuarterView 3D Action BE5/Script/Player.cs
+++ Assets/QuarterView 3D Action BE5/Script/Player.cs
@@ -4,33 +4,70 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float Speed;
+    public float Speed;//캐릭터 속도 변수
     float hAxis;
     float vAxis;
-    bool wDown;
+    bool wDown;//걷기버튼 변수
+    bool jDown;//점프버튼 변수
+
+    bool isJump; //지금 점프를 하는지에대해 검사하는 변수
 
     Vector3 moveVec;
-    Animator anim;
+    Animator anim;//자식관계에서 활동중 컴포넌트 차일드 활용
+    Rigidbody rigid;
 
     void Awake()
     {
         anim = GetComponentInChildren<Animator>();
+        rigid = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+    }
+
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
+        jDown = Input.GetButtonDown("Jump");
+    }
 
-        moveVec = new Vector3(hAxis, 0 ,vAxis).normalized;
+    void Move()
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
-        transform.position += moveVec * Speed * (wDown ? 0.3f : 1 ) * Time.deltaTime;
-        
+        transform.position += moveVec * Speed * (wDown ? 0.3f : 1) * Time.deltaTime;
 
-        anim.SetBool("isRun",moveVec != Vector3.zero);
+
+        anim.SetBool("isRun", moveVec != Vector3.zero);
         anim.SetBool("isWalk", wDown);
+    }
 
+    void Turn()
+    {
         transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()
+    { 
+        if (jDown && !isJump)
+        {
+            rigid.AddForce(Vector3.up * 15, ForceMode.Impulse);//임펄스는 즉발적인 힘
+            isJump = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            isJump = false;
+        }
     }
 }
